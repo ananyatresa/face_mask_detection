67c5a5684da8eaec652c67341fbfa864168b7749
Anand Salil <52151136+glitch-anand@users.noreply.github.com>
2021-04-03 21:30:20 +0530

    Removed error comments

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -61,19 +61,7 @@ def button1_clicked(m):
 
 
 		return (locs, preds)
-	'''
-	ap = argparse.ArgumentParser()
-	ap.add_argument("-f", "--face", type=str,
-		default="face_detector",
-		help="path to face detector model directory")
-	ap.add_argument("-m", "--model", type=str,
-		default="mask_detector.model",
-		help="path to trained face mask detector model")
-	ap.add_argument("-c", "--confidence", type=float, default=0.5,
-		help="minimum probability to filter weak detections")
-	args = vars(ap.parse_args())
-
-	'''
+	
 
 	print("loading face detector model...")
 	prototxtPath = r"deploy.prototxt"
