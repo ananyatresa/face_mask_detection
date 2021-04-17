31ac5dc74a8fe61d1c313259182386e45931bd41
Anand Salil <52151136+glitch-anand@users.noreply.github.com>
2021-04-17 15:28:42 +0530

    driver script
    executes cluster_faces after encode_faces finishes executing

--- driver_script.py
+++ driver_script.py
@@ -1,7 +1,6 @@
 import subprocess
 
 program_list = ['encode_faces.py', 'cluster_faces.py']
-
 for program in program_list:
     subprocess.call(['python', program])
     print("Finished:" + program)
