130b45791d209bd8203363727fa621a902ad9e8e
Anand Salil <52151136+glitch-anand@users.noreply.github.com>
2021-04-21 13:14:09 +0530

    Delete driver_script.py

--- driver_script.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-program_list = ['encode_faces.py', 'cluster_faces.py','delete_faces.py']
-
-for program in program_list:
-    subprocess.call(['python', program])
-    print("Finished:" + program)
